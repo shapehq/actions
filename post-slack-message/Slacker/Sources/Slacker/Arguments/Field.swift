72bc988123654448720796a4ef96bb51e8c29a0f
Mathias Emil Mortensen <mathias@shape.dk>
2023-03-02 12:57:16 +0100

    Updates field argument parsing

--- post-slack-message/Slacker/Sources/Slacker/Arguments/Field.swift
+++ post-slack-message/Slacker/Sources/Slacker/Arguments/Field.swift
@@ -13,9 +13,9 @@ struct Field: ExpressibleByArgument {
     let value: String
 
     init?(argument: String) {
-        let components = argument.split(separator: ":")
-        assert(components.count == 2, "A SlackField must be composed of a title and a value separated by a colon. Example: 'Branch:main'")
-        title = String(components[0])
-        value = String(components[1])
+        let comps = argument.split(separator: ":")
+        assert(comps.count >= 2, "A SlackField must be composed of a title and a value separated by a colon. Example: 'Branch:main'")
+        title = String(comps[0])
+        value = comps[comps.startIndex.advanced(by: 1) ..< comps.endIndex].joined(separator: ":")
     }
 }
