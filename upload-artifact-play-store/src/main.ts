7316e6b1061c721df309197048932ff6dc49ec73
Gerard Sala <gerard@shape.dk>
2023-06-08 14:50:52 +0200

    First go at publishing an app

--- upload-artifact-play-store/src/main.ts
+++ upload-artifact-play-store/src/main.ts
@@ -1,19 +1,200 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as google from "@googleapis/androidpublisher";
+import * as fs from "fs";
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+import { androidpublisher_v3 } from "@googleapis/androidpublisher";
+import Publisher = androidpublisher_v3.Androidpublisher;
+import Apk = androidpublisher_v3.Schema$Apk;
+import Bundle = androidpublisher_v3.Schema$Bundle;
+import Track = androidpublisher_v3.Schema$Track;
+import { GoogleAuth } from "google-auth-library";
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+async function createAuthClient(serviceAccountKeyPath: string): Promise<GoogleAuth> {
+  return new google.auth.GoogleAuth({
+    keyFile: serviceAccountKeyPath,
+    scopes: ["https://www.googleapis.com/auth/androidpublisher"],
+  });
+}
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+async function publishApp(
+  packageName: string,
+  serviceAccountKeyPath: string,
+  bundlePath: string,
+  proguardMappingFilePath: string
+): Promise<void> {
+  const authClient = await createAuthClient(serviceAccountKeyPath);
+  const publisher = google.androidpublisher({
+    version: "v3",
+    auth: authClient,
+  });
+
+  const editId = await createEdit(publisher, packageName);
+  validateSelectedTrack(publisher, editId, packageName);
+
+  const versionCode = await uploadReleaseFiles(publisher, editId, packageName, bundlePath);
+  if (proguardMappingFilePath) {
+    await uploadProguardMappingFile(publisher, editId, packageName, versionCode, proguardMappingFilePath);
+  }
+  const track = await updateTrack(publisher, editId, packageName, versionCode);
+  await commitEdit(publisher, editId, packageName);
+}
+
+async function createEdit(publisher: Publisher, packageName: string): Promise<string> {
+  core.debug(`Creating new Edit for ${packageName}`);
+  const result = await publisher.edits.insert({ packageName });
+  if (result.status != 200) {
+    throw Error(result.statusText);
+  }
+  if (!result.data.id) {
+    throw Error("Something went wrong.");
+  }
+  core.debug(`Created new Edit for ${packageName} - Expires at ${String(result.data.expiryTimeSeconds)}`);
+  return result.data.id;
+}
+
+async function validateSelectedTrack(publisher: Publisher, editId: string, packageName: string, track: string = "internal"): Promise<void> {
+  core.info(`Validating track "${track}"`);
+  const res = await publisher.edits.tracks.list({
+    editId: editId,
+    packageName: packageName,
+  });
+  if (res.status != 200) {
+    throw Error(res.statusText);
+  }
+  const allTracks = res.data.tracks;
+  // Check whether we actually have any tracks
+  if (!allTracks) {
+    throw Error("No tracks found, unable to validate track.");
+  }
+
+  // Check whether the track is valid
+  if (allTracks.find((value) => value.track == track) == undefined) {
+    const allTrackNames = allTracks.map((track) => {
+      return track.track;
+    });
+    throw Error(`Track "${track}" could not be found. Available tracks are: ${allTrackNames.toString()}`);
   }
 }
 
-run()
+async function uploadReleaseFiles(publisher: Publisher, editId: string, packageName: string, releaseFile: string): Promise<number> {
+  core.info(`Uploading ${releaseFile}`);
+  if (releaseFile.endsWith(".apk")) {
+    // Upload APK, or throw when something goes wrong
+    const apk = await uploadApk(publisher, editId, packageName, releaseFile);
+    if (!apk.versionCode) throw Error("Failed to upload APK.");
+    core.info(`Uploaded APK with version code ${apk.versionCode}`);
+    return apk.versionCode;
+  } else if (releaseFile.endsWith(".aab")) {
+    // Upload AAB, or throw when something goes wrong
+    const bundle = await uploadBundle(publisher, editId, packageName, releaseFile);
+    if (!bundle.versionCode) throw Error("Failed to upload bundle.");
+    core.info(`Uploaded bundle with version code ${bundle.versionCode}`);
+    return bundle.versionCode;
+  } else {
+    // Throw if file extension is not right
+    throw Error(`${releaseFile} is invalid.`);
+  }
+}
+
+async function updateTrack(
+  publisher: Publisher,
+  editId: string,
+  packageName: string,
+  versionCode: number,
+  track: string = "internal"
+): Promise<Track> {
+  core.info(`Updating track "${track}" in "${packageName}" with build "${versionCode}"`);
+  const res = await publisher.edits.tracks.update({
+    packageName: packageName,
+    editId: editId,
+    track: track,
+    requestBody: {
+      track: track,
+      releases: [
+        {
+          versionCodes: [versionCode.toString()],
+          status: "completed",
+        },
+      ],
+    },
+  });
+  return res.data;
+}
+
+async function commitEdit(publisher: Publisher, editId: string, packageName: string): Promise<string> {
+  core.info(`Committing the Edit`);
+  const res = await publisher.edits.commit({
+    editId: editId,
+    packageName: packageName,
+  });
+
+  if (res.data.id) {
+    core.info(`Successfully committed ${res.data.id}`);
+    return res.data.id;
+  } else {
+    return Promise.reject(res.status);
+  }
+}
+
+async function uploadBundle(publisher: Publisher, editId: string, packageName: string, bundleReleaseFile: string): Promise<Bundle> {
+  core.debug(`Uploading App Bundle @ ${bundleReleaseFile}`);
+  const res = await publisher.edits.bundles.upload({
+    packageName: packageName,
+    editId: editId,
+    media: {
+      mimeType: "application/octet-stream",
+      body: fs.createReadStream(bundleReleaseFile),
+    },
+  });
+  return res.data;
+}
+
+async function uploadApk(publisher: Publisher, editId: string, packageName: string, apkReleaseFile: string): Promise<Apk> {
+  core.debug(`Uploading APK @ ${apkReleaseFile}`);
+  const res = await publisher.edits.apks.upload({
+    packageName: packageName,
+    editId: editId,
+    media: {
+      mimeType: "application/vnd.android.package-archive",
+      body: fs.createReadStream(apkReleaseFile),
+    },
+  });
+  return res.data;
+}
+
+async function uploadProguardMappingFile(
+  publisher: Publisher,
+  editId: string,
+  packageName: string,
+  versionCode: number,
+  mappingFile: string
+) {
+  if (mappingFile != undefined && mappingFile.length > 0) {
+    core.debug(`Uploading Proguard mapping file @ ${mappingFile}`);
+    await publisher.edits.deobfuscationfiles.upload({
+      packageName: packageName,
+      editId: editId,
+      apkVersionCode: versionCode,
+      deobfuscationFileType: "proguard",
+      media: {
+        mimeType: "application/octet-stream",
+        body: fs.createReadStream(mappingFile),
+      },
+    });
+  }
+}
+
+async function run(): Promise<void> {
+  try {
+    const serviceAccountKeyPath = core.getInput("serviceAccountKeyPath", { required: true });
+    const packageName = core.getInput("packageName", { required: true });
+    const bundlePath = core.getInput("bundlePath", { required: true });
+    const proguardMappingFilePath = core.getInput("proguardMappingFilePath");
+
+    publishApp(serviceAccountKeyPath, packageName, bundlePath, proguardMappingFilePath);
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+}
+
+run();
