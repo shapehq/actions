46bb4993ac24b2d89d045dd15fe3d82b86aefaf6
Simon Støvring <mail@simonbs.dk>
2023-11-20 12:53:41 +0100

    Adds SwiftLint action

---
+++ swiftlint/main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Inspired by the norio-nomura/action-swiftlint action.
+# https://github.com/norio-nomura/action-swiftlint
+
+function stripPWD() {
+  if ! ${WORKING_DIRECTORY+false}; then
+    cd - > /dev/null
+  fi
+  sed -E "s/$(pwd|sed 's/\//\\\//g')\///"
+}
+
+if ! ${WORKING_DIRECTORY+false}; then
+  cd ${WORKING_DIRECTORY}
+fi
+
+set -o pipefail && swiftlint "$@" --reporter github-actions-logging -- $changedFiles | stripPWD
