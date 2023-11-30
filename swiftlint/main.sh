c753ebf9607c81b1d3f19a8df539e2617ec8b6e1
Simon B. Støvring <mail@simonbs.dk>
2023-11-30 10:47:26 +0100

    Update main.sh

--- swiftlint/main.sh
+++ swiftlint/main.sh
@@ -16,7 +16,7 @@ fi
 
 if ! ${DIFF_BASE+false}; then
   changedFiles=$(git --no-pager diff --name-only --relative FETCH_HEAD $(git merge-base FETCH_HEAD $DIFF_BASE) -- '*.swift')
-  if [ -z "$changedFiles" ] then
+  if [ -z "$changedFiles" ]; then
     echo "No Swift file changed"
     exit
   fi
