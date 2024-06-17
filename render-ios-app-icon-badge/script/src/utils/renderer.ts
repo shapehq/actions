9a88e578a6cff2b8ecb00a9c5b39801105314118
Simon Støvring <mail@simonbs.dk>
2024-06-17 22:58:34 +0200

    Reverts renderer

--- render-ios-app-icon-badge/script/src/utils/renderer.ts
+++ render-ios-app-icon-badge/script/src/utils/renderer.ts
@@ -1,6 +1,4 @@
-// import { createCanvas, loadImage, Image } from "canvas"
-import gm from "gm"
-const im = gm.subClass({ imageMagick: true })
+import { createCanvas, loadImage, Image } from "canvas"
 
 export async function recolorPixels(
   image: Image, 
@@ -23,37 +21,37 @@ export async function recolorPixels(
   return await loadImage(buffer)
 }
 
-// export async function renderLayers(
-//   images: Image[], 
-//   targetSize: { width: number, height: number }
-// ) {
-//   const canvas = createCanvas(targetSize.width, targetSize.height)
-//   const ctx = canvas.getContext("2d")
-//   for (const image of images) {
-//     ctx.drawImage(image, 0, 0, targetSize.width, targetSize.height)
-//   }
-//   const buffer = canvas.toBuffer("image/png")
-//   return await loadImage(buffer)
-// }
-// 
-// export function getPixelColor(image: Image, point: { x: number, y: number }) {
-//   if (point.x < 0 || point.x > image.width) {
-//     throw new Error("point.x must be within the width of the image")
-//   }
-//   if (point.y < 0 || point.y > image.height) {
-//     throw new Error("point.y must be within the height of the image")
-//   }
-//   const canvas = createCanvas(image.width, image.height)
-//   const ctx = canvas.getContext("2d")
-//   ctx.drawImage(image, 0, 0, image.width, image.height)
-//   const pixel = ctx.getImageData(point.x, point.y, 1, 1)
-//   const pixelData = pixel.data
-//   return { r: pixelData[0], g: pixelData[1], b: pixelData[2] }
-// }
-// 
-// export function renderToBuffer(image: Image) {
-//   const canvas = createCanvas(image.width, image.height)
-//   const ctx = canvas.getContext("2d")
-//   ctx.drawImage(image, 0, 0, image.width, image.height)
-//   return canvas.toBuffer("image/png")
-// }
+export async function renderLayers(
+  images: Image[], 
+  targetSize: { width: number, height: number }
+) {
+  const canvas = createCanvas(targetSize.width, targetSize.height)
+  const ctx = canvas.getContext("2d")
+  for (const image of images) {
+    ctx.drawImage(image, 0, 0, targetSize.width, targetSize.height)
+  }
+  const buffer = canvas.toBuffer("image/png")
+  return await loadImage(buffer)
+}
+
+export function getPixelColor(image: Image, point: { x: number, y: number }) {
+  if (point.x < 0 || point.x > image.width) {
+    throw new Error("point.x must be within the width of the image")
+  }
+  if (point.y < 0 || point.y > image.height) {
+    throw new Error("point.y must be within the height of the image")
+  }
+  const canvas = createCanvas(image.width, image.height)
+  const ctx = canvas.getContext("2d")
+  ctx.drawImage(image, 0, 0, image.width, image.height)
+  const pixel = ctx.getImageData(point.x, point.y, 1, 1)
+  const pixelData = pixel.data
+  return { r: pixelData[0], g: pixelData[1], b: pixelData[2] }
+}
+
+export function renderToBuffer(image: Image) {
+  const canvas = createCanvas(image.width, image.height)
+  const ctx = canvas.getContext("2d")
+  ctx.drawImage(image, 0, 0, image.width, image.height)
+  return canvas.toBuffer("image/png")
+}
