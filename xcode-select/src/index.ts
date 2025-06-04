3cc59823aca08ac51193d3306a77c21c436f0e63
Simon Støvring <mail@simonbs.dk>
2025-06-04 13:20:06 +0200

    Removes CompositionRoot type

--- xcode-select/src/index.ts
+++ xcode-select/src/index.ts
@@ -1,8 +1,30 @@
-import CompositionRoot from "./CompositionRoot"
+import * as core from "@actions/core"
 import getOptions from "./getOptions"
+import Action from "./Action"
+import LoggerLive from "./Logger/LoggerLive"
+import KeyValueStateStore from "./StateStore/KeyValueStateStore"
+import CommandRunnerLive from "./CommandRunner/CommandRunnerLive"
+import FileSystemLive from "./FileSystem/FileSystemLive"
+import SemanticVersionParser from "./SemanticVersion/SemanticVersionParser"
+import SemanticVersionTemplateParserLive from "./SemanticVersion/SemanticVersionTemplateParserLive"
+import XcodeVersionMatcher from "./XcodeVersion/XcodeVersionMatcher"
+import XcodeVersionParserLive from "./XcodeVersion/XcodeVersionParserLive"
+import XcodeVersionRepositoryLive from "./XcodeVersion/XcodeVersionRepositoryLive"
+import XcodeSelectorLive from "./XcodeSelector/XcodeSelectorLive"
 
-const action = CompositionRoot.getAction()
-const logger = CompositionRoot.getLogger()
+const logger = new LoggerLive()
+const xcodeVersionRepository = new XcodeVersionRepositoryLive(
+  new FileSystemLive(), 
+  new XcodeVersionParserLive(new SemanticVersionParser())
+)
+const action = new Action(
+  new KeyValueStateStore(core),
+  logger,
+  new SemanticVersionTemplateParserLive(),
+  xcodeVersionRepository,
+  new XcodeVersionMatcher(xcodeVersionRepository),
+  new XcodeSelectorLive(new CommandRunnerLive())
+)
 action.run(getOptions()).catch(err => {
   logger.error(err.toString())
 })
