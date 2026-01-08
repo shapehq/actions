c4689081d23567f0185498934435dbe93c4e7c2b
Simon Støvring <mail@simonbs.dk>
2026-01-08 13:49:27 +0100

    Updates ICommandRunner to take array of args

--- xcode-select/src/CommandRunner/ExecCommandRunner.ts
+++ xcode-select/src/CommandRunner/ExecCommandRunner.ts
@@ -1,30 +1,18 @@
-import { exec, execFile } from "child_process"
+import { execFile } from "child_process"
 import ICommandRunner from "./ICommandRunner"
 
 export default class ExecCommandRunner implements ICommandRunner {
-  run(cmd: string | string[]): Promise<string> {
+  run(cmd: string[]): Promise<string> {
     return new Promise((resolve, reject) => {
-      if (Array.isArray(cmd)) {
-        execFile(cmd[0], cmd.slice(1), (error, stdout, stderr) => {
-          if (error) {
-            reject(error)
-          } else if (stderr) {
-            reject(stderr)
-          } else {
-            resolve(stdout)
-          }
-        })
-      } else {
-        exec(cmd, (error, stdout, stderr) => {
-          if (error) {
-            reject(error)
-          } else if (stderr) {
-            reject(stderr)
-          } else {
-            resolve(stdout)
-          }
-        })
-      }
+      execFile(cmd[0], cmd.slice(1), (error, stdout, stderr) => {
+        if (error) {
+          reject(error)
+        } else if (stderr) {
+          reject(stderr)
+        } else {
+          resolve(stdout)
+        }
+      })
     })
   }
 }
